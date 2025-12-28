c332daf75a2f8fdd78e4f74a2e0ecb4439000341
AArt1256 <aart1256@outlook.com>
2025-12-28 17:53:30 +0300

    added files 2

---
+++ main.cpp
@@ -0,0 +1,324 @@
+#define GL_GLEXT_PROTOTYPES
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <SDL.h>
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <SDL_opengles2.h>
+// TODO: add glext include here
+#else
+#include <SDL_opengl.h>
+#endif
+
+// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
+#ifdef __EMSCRIPTEN__
+#include "../libs/emscripten/emscripten_mainloop_stub.h"
+#endif
+
+#define rightClickable if (ImGui::IsItemClicked(ImGuiMouseButton_Right)) ImGui::SetKeyboardFocusHere(-1);
+
+void save_settings();
+void load_settings();
+
+struct window_bool {
+    bool settings;
+    bool imgui_debugger;
+};
+
+struct window_bool visible_windows; // misc.h
+
+static bool opt_padding = false; // Is there padding (a blank space) between the window edge and the Dockspace?
+
+void ShowExampleAppDockSpace(bool* p_open) {
+    // Variables to configure the Dockspace example.
+    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None; // Config flags for the Dockspace
+
+    // In this example, we're embedding the Dockspace into an invisible parent window to make it more configurable.
+    // We set ImGuiWindowFlags_NoDocking to make sure the parent isn't dockable into because this is handled by the Dockspace.
+    //
+    // ImGuiWindowFlags_MenuBar is to show a menu bar with config options. This isn't necessary to the functionality of a
+    // Dockspace, but it is here to provide a way to change the configuration flags interactively.
+    // You can remove the MenuBar flag if you don't want it in your app, but also remember to remove the code which actually
+    // renders the menu bar, found at the end of this function.
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+
+    // Is the example in Fullscreen mode?
+    {
+        // If so, get the main viewport:
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
+
+        // Set the parent window's position, size, and viewport to match that of the main viewport. This is so the parent window
+        // completely covers the main viewport, giving it a "full-screen" feel.
+        ImGui::SetNextWindowPos(viewport->WorkPos);
+        ImGui::SetNextWindowSize(viewport->WorkSize);
+        ImGui::SetNextWindowViewport(viewport->ID);
+
+        // Set the parent window's styles to match that of the main viewport:
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f); // No corner rounding on the window
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f); // No border around the window
+
+        // Manipulate the window flags to make it inaccessible to the user (no titlebar, resize/move, or navigation)
+        window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+        window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+    }
+
+    // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+    // and handle the pass-thru hole, so the parent window should not have its own background:
+    if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+        window_flags |= ImGuiWindowFlags_NoBackground;
+
+    // If the padding option is disabled, set the parent window's padding size to 0 to effectively hide said padding.
+    if (!opt_padding)
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+
+    // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+    // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+    // all active windows docked into it will lose their parent and become undocked.
+    // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+    // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+    ImGui::Begin("DockSpace Demo", p_open, window_flags);
+
+    // Remove the padding configuration - we pushed it, now we pop it:
+    if (!opt_padding)
+        ImGui::PopStyleVar();
+
+    // Pop the two style rules set in Fullscreen mode - the corner rounding and the border size.
+    ImGui::PopStyleVar(2);
+
+    // Check if Docking is enabled:
+    ImGuiIO& io = ImGui::GetIO();
+    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+    {
+        // If it is, draw the Dockspace with the DockSpace() function.
+        // The GetID() function is to give a unique identifier to the Dockspace - here, it's "MyDockSpace".
+        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+    }
+    else
+    {
+        // Docking is DISABLED - Show a warning message
+    }
+
+    // This is to show the menu bar that will change the config settings at runtime.
+    // If you copied this demo function into your own code and removed ImGuiWindowFlags_MenuBar at the top of the function,
+    // you should remove the below if-statement as well.
+    if (ImGui::BeginMenuBar())
+    {
+        if (ImGui::BeginMenu("file")) {
+            // Disabling fullscreen would allow the window to be moved to the front of other windows,
+            // which we can't undo at the moment without finer window depth/z control.
+            if (ImGui::MenuItem("open file")) {
+                /*
+                audio_paused = true;
+                auto f = pfd::open_file("Choose a file to read", pfd::path::home(),
+                            { "Mah-Tracker Files", "*.mah" },
+                            pfd::opt::none).result();
+                if (!f.empty()) {
+                    load_file((char *)f[0].c_str());
+                    audio_paused = false;
+                    reset_audio_buffer_and_unpause();
+                } else {
+                    audio_paused = false;
+                    reset_audio_buffer_and_unpause();
+                }
+                */
+            }
+
+            ImGui::EndMenu();
+        }
+
+        ImGui::MenuItem("settings", NULL, (bool *)&visible_windows.settings);
+
+        if (ImGui::BeginMenu("windows")) {
+            ImGui::MenuItem("ImGui Debugger", NULL, (bool *)&visible_windows.imgui_debugger);
+            ImGui::EndMenu();
+        }
+
+
+        ImGui::EndMenuBar();
+    }
+
+    // End the parent window that contains the Dockspace:
+    ImGui::End();
+}
+
+// save settings
+void save_settings() {
+    ImGuiIO& io = ImGui::GetIO();
+    std::string filename = "mah-tracker.cfg";
+    std::ofstream f(filename);
+    if (!f) return;
+    std::string value;
+    f << io.FontGlobalScale << "\n";
+    for (int i = 0; i < sizeof(visible_windows); i++) {
+        f << (int)(((uint8_t*)&visible_windows)[i]) << "\n";
+    }
+}
+
+// load settings
+void load_settings() {
+    ImGuiIO& io = ImGui::GetIO();
+    std::string filename = "mah-tracker.cfg";
+    std::ifstream f(filename);
+    if (!f) return;
+    std::string value;
+    getline(f,value); if (!value.empty()) io.FontGlobalScale = std::stof(value);
+    for (int i = 0; i < sizeof(visible_windows); i++) {
+        getline(f,value); if (!value.empty()) ((uint8_t*)&visible_windows)[i] = std::stoi(value);
+    }
+}
+
+// Main code
+int main(int argc, char *argv[]) {
+    visible_windows.imgui_debugger = false;
+    
+    // Setup SDL
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
+    {
+        printf("Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100 (WebGL 1.0)
+    const char* glsl_version = "#version 100";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#elif defined(IMGUI_IMPL_OPENGL_ES3)
+    // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
+    const char* glsl_version = "#version 300 es";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#elif defined(__APPLE__)
+    // GL 3.2 Core + GLSL 150
+    const char* glsl_version = "#version 150";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#endif
+
+    // From 2.0.18: Enable native IME.
+#ifdef SDL_HINT_IME_SHOW_UI
+    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+#endif
+
+    // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI| SDL_WINDOW_MAXIMIZED);
+    SDL_Window* window = SDL_CreateWindow("myp6502", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    if (window == nullptr)
+    {
+        printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
+        return -1;
+    }
+
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    if (gl_context == nullptr)
+    {
+        printf("Error: SDL_GL_CreateContext(): %s\n", SDL_GetError());
+        return -1;
+    }
+
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("IBMPlexSans.ttf", 25.0f); // FOINISS FONT OH MAI GAHHHH
+
+    io.FontGlobalScale = 1.0f;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+
+    // Setup Dear ImGui style
+    //ImGui::StyleColorsDark();
+    ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Our state
+    ImVec4 clear_color = ImVec4(0.45f, 0.45f, 0.45f, 1.00f);
+
+    load_settings();
+    save_settings(); // for updating config files from older versions
+
+    // Main loop
+    bool done = false;
+    int cur_frame = 0;
+#ifdef __EMSCRIPTEN__
+    // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
+    // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
+    io.IniFilename = nullptr;
+    EMSCRIPTEN_MAINLOOP_BEGIN
+#else
+    // Main loop
+    while (!has_quit()) {
+#endif
+
+        cur_frame++;
+        if (cur_frame == (50*60)) {
+            cur_frame = 0;
+            save_settings();
+        }
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        ShowExampleAppDockSpace((bool*)false);
+
+        if (visible_windows.imgui_debugger) {
+            ImGui::ShowMetricsWindow((bool *)&visible_windows.imgui_debugger);
+        }
+
+        ImGui::Render();
+
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+#ifdef __EMSCRIPTEN__
+    EMSCRIPTEN_MAINLOOP_END;
+#endif
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
+    return 0;
+}
